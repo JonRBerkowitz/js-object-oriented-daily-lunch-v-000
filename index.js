24cde4bf1789a43a770e4df3a203fbb278db1506
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-13 23:58:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
+class Neighborhood {
+  constructor(name) {
+    this.id = neighborhoodId++;
+    this.name = name;
+    store.neighborhoods.push(this);
+  }
+}
+
+class Customer {
+  constructor(name, neighborhood) {
+    this.id = customerId++;
+    this.name = name;
+    this.neighborhoodId = neighborhood.id;
+    store.customers.push(this);
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.id = mealId++;
+    this.title = title;
+    this.price = price;
+    store.meals.push(this);
+  }
+}
+
+class Delivery {
+
+}
