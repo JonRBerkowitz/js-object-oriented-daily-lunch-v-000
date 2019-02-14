e9d5aefaab247ca2e1135a99ce6bcd720266c564
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 00:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ class Meal {
 
 class Delivery {
   constructor(mealId, neighborhoodId, customerId) {
+    this.id = deliveryIds;
     this.mealId = mealId;
     this.neighborhoodId = neighborhoodId;
     this.customerId = customerId;
