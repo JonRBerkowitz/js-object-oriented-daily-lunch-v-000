5877e1b8cae866b41192d3bbfe90524842d149e3
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 01:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,12 +56,9 @@ class Meal {
     });
   }
   customers() {
-    let customerArr = []
-    this.deliveries().map(delivery => {
-      customerArr.push(delivery.customer());
+    return this.deliveries().map(delivery => {
+      return delivery.customer();
     });
-    return customerArr;
-  }
 }
 
 class Delivery {
