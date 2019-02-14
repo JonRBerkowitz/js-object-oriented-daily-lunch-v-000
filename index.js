5ad2a47d1a0b7b7351dded9e6cf707bba28af4e0
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 00:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ class Neighborhood {
     store.neighborhoods.push(this);
   }
 }
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.id = customerIds++;
+    this.name = name;
+    this.neighborhoodId = neighborhoodId;
+    store.customers.push(this);
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.id = mealIds++;
+    this.title = title;
+    this.price = price;
+    store.meals.push(this);
+  }
+}
+
+class Delivery {
+  constructor(mealId, neighborhoodId, customerId) {
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+    store.delivers.push(this);
+  }
+}
